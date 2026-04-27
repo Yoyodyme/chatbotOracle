3399aa4f7c6468c258c62aa8d33b6131719855b3
Gabriel <a00839889@tec.mx>
2026-04-27 08:24:10 -0600

    fix: Remove hardcoded OCI credentials and add SQLFluff config

--- MtdrSpring/terraform/main-var.tf
+++ MtdrSpring/terraform/main-var.tf
@@ -1,8 +1,10 @@
 //Copyright (c) 2022 Oracle and/or its affiliates.
 //Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-variable "ociTenancyOcid"      { default = "ocid1.tenancy.oc1..aaaaaaaab32jpf5nepirstbkntsg4kuv45ntdluw4izzvmvs6wgutphfrepq" }
-variable "ociUserOcid"         { default = "ocid1.user.oc1..aaaaaaaay65zakgqjef5ottuzlq6lo4oivlcmmgymhpczg2fdgisuhgiu6eq" }
-variable "ociCompartmentOcid"  { default = "ocid1.compartment.oc1..aaaaaaaao6cvbreyw2kk66wnig3i7ukedb22cmflvhmvg5mpflbiurmj5eoa" }
+# These values should be supplied via terraform.tfvars or environment variables
+# to avoid committing tenancy/user/compartment identifiers in the repository.
+variable "ociTenancyOcid"      { default = "" }
+variable "ociUserOcid"         { default = "" }
+variable "ociCompartmentOcid"  { default = "" }
 variable "ociRegionIdentifier" { default = "mx-queretaro-1" }
 variable "mtdrDbName"          { default = "chatbotdb" }
 variable "runName"             { default = "chatbotoracle" }
